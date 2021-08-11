431138646aaa098d8d6427d988a21db3a063d3f1
heusserm <matt.heusser@gmail.com>
2021-08-11 10:15:57 -0400

    make code more idiomatic ruby

--- ruby_selenium_example/test_wonderproxy.rb
+++ ruby_selenium_example/test_wonderproxy.rb
@@ -10,19 +10,16 @@ require 'webdrivers'
 class WonderProxyTest < MiniTest::Test
   
   def setup 
-    #----------------------------------------------
     @browser = Selenium::WebDriver.for :chrome
-    #                             ^----> change to :firefox, :safari etc as needed
-    #---------------------------------------------
     @browser.manage.timeouts.implicit_wait = 30 
-    @wait = Selenium::WebDriver::Wait.new(:timeout => 15);
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
   end
 
   def object_loaded(object)
     if (object.nil? || object == 0 || (!(object.class.name.eql? "Selenium::WebDriver::Element")))
-      return false;
+      return false
     else
-      return true;
+      return true
     end
   end
 
@@ -31,27 +28,29 @@ class WonderProxyTest < MiniTest::Test
       element = @browser.find_element(:xpath => locator)
       element if element.displayed?
     }
-    return link;
+    return link
   end
 
   def test_wonderproxy
     @browser.navigate.to "https://wonderproxy.com"
-    assert((@browser.title.eql? "Localization testing with confidence - WonderProxy"), "Title on homepage should be standard");
+    assert((@browser.title.eql? "Localization testing with confidence - WonderProxy"), "Title on homepage should be standard")
   end
 
   def test_wonderproxy_plans
     #Click explore plans, see text!
     @browser.navigate.to "https://wonderproxy.com"
-    link = locate_object( "//a[text()='Explore Plans']"); 
-    assert(object_loaded(link),"Found explore plans link on homepage");   
-    link.click();
-    assert((@browser.page_source.include? "Start localization testing today."),"Text should be signup page text after click");
-    assert((@browser.page_source.include? "Get accurate results."),"Checkng additional signup text");
+    link = locate_object( "//a[text()='Explore Plans']") 
+    assert(object_loaded(link),"Found explore plans link on homepage")   
+    link.click()
+    assert((@browser.page_source.include? "Start localization testing today."),
+            "Text should be signup page text after click")
+ 
+    assert((@browser.page_source.include? "Get accurate results."),
+            "Checkng additional signup text")
   end
 
-
   def teardown
-    @browser.close();
+    @browser.close()
   end
 
 end
